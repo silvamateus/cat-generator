f8476dc1a169527d20e08037b1026bef60bc9ff9
silvamateus <mateus.wtv@gmail.com>
2018-04-08 09:11:09 -0300

    Start the feed

---
+++ src/components/CatFeed.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+      <header>How do you like your cats?</header>
+      <div>
+          <img :src="whiskasFeed" :alt="whiskasFeed">
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CatFeed',
+  props:{
+    'whiskasFeed' : String
+  }
+}
+</script>
