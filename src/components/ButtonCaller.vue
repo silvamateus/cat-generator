12bf0ece44571d79272cf8f856f6d800da460bbc
silvamateus <mateus.wtv@gmail.com>
2018-04-09 10:23:42 -0300

    Style ButtonCaller button

--- src/components/ButtonCaller.vue
+++ src/components/ButtonCaller.vue
@@ -22,7 +22,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   button{
-    width: 10rem;
-    height: 10rem;
+    text-align: center;
+    white-space: nowrap;
+    max-width: 480px;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+    padding-left: calc(389px/2);
+    padding-right: calc(400px/2);
+    border: 0;
+    background-color: #03A678;
+    color: #ECF0F1;
+    transition: opacity 1s;
+    &:disabled, &:disabled:hover {
+      cursor: not-allowed;
+      opacity: .4;
+    }
+    &:hover {
+      opacity: .7;
+    }
   }
 </style>
