1507c61d853c01444f41293e07d6811fcadcff99
silvamateus <mateus.wtv@gmail.com>
2018-04-09 10:20:57 -0300

    Add default box-sizing and font behavior

--- src/App.vue
+++ src/App.vue
@@ -41,11 +41,13 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-family: 'Roboto', sans-serif;
+
 }
 #cat-header, footer {
   background-color: #fefefe;
